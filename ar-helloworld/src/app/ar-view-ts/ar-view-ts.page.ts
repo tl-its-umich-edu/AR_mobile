da6785790c0a76e8449199029818cd3bd28ba616
Alex Kim <akim8@emich.edu>
2019-11-26 08:07:30 -0500

    Try to get aframe to work by injecting html

--- ar-helloworld/src/app/ar-view-ts/ar-view-ts.page.ts
+++ ar-helloworld/src/app/ar-view-ts/ar-view-ts.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { $ } from 'protractor';
 import { AFrame } from 'aframe';
 
@@ -12,9 +12,16 @@ export class ArViewTsPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
-
     require('aframe');
-
     
+    document.getElementById('ar-container').innerHTML = `
+    <a-scene>
+      <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+      <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+      <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
+      <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+      <a-sky color="#ECECEC"></a-sky>
+    </a-scene>
+    `;
   }
 }
