661e885b40495ff9f25b86b1e99771b88005e2a8
pushyami <pushyami@umich.edu>
2019-12-16 08:28:07 -0500

    Adding support for AR with Android device

--- src/app/ar-busstops/ar-busstops.page.ts
+++ src/app/ar-busstops/ar-busstops.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Platform } from '@ionic/angular';
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
 @Component({
   selector: 'app-ar-busstops',
   templateUrl: './ar-busstops.page.html',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArBusstopsPage implements OnInit {
 
-  constructor() { }
+  constructor(private platform: Platform, private androidPermissions: AndroidPermissions) {
+    if (this.platform.is('cordova')) {
+      console.log("android setting")
+      this.platform.ready().then(() => {
+        this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.CAMERA).then(
+            result => console.log('Has permission?', result.hasPermission),
+            err => this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.CAMERA)
+        );
+
+        this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.CAMERA,
+          this.androidPermissions.PERMISSION.GET_ACCOUNTS]);
+      });
+    }
+  }
 
   ngOnInit() {
   }
--- src/app/ar-view/ar-view.page.ts
+++ src/app/ar-view/ar-view.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
+
 
 @Component({
   selector: 'app-ar-view',
@@ -7,7 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArViewPage implements OnInit {
 
-  constructor() { }
+  constructor(private platform: Platform, private androidPermissions: AndroidPermissions) {
+    if (this.platform.is('cordova')) {
+      console.log("android setting")
+      this.platform.ready().then(() => {
+        this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.CAMERA).then(
+            result => console.log('Has permission?', result.hasPermission),
+            err => this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.CAMERA)
+        );
+
+        this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.CAMERA,
+          this.androidPermissions.PERMISSION.GET_ACCOUNTS]);
+      });
+    }
+  }
 
   ngOnInit() {
   }
