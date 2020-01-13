012fc275d769c03afa00d1e6f0fbbf50176a8364
Alex Kim <akim8@emich.edu>
2020-01-13 09:42:12 -0500

    update readme and request camera/gps permissions at the same time

--- src/app/ar-busstops/ar-busstops.page.ts
+++ src/app/ar-busstops/ar-busstops.page.ts
@@ -11,43 +11,32 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
   templateUrl: './ar-busstops.page.html',
   styleUrls: ['./ar-busstops.page.scss'],
 })
-export class ArBusstopsPage implements OnInit {
+export class ArBusstopsPage {
 
   url: SafeResourceUrl;
   apiKey: string;
 
   constructor(private platform: Platform, private androidPermissions: AndroidPermissions, private geolocation: Geolocation, private sanitizer: DomSanitizer) {
+
+    // get device permissions
     if (this.platform.is('cordova')) {
-      
-      // camera permissions
-      
       this.platform.ready().then(() => {
-        this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.CAMERA).then(
-            result => console.log('Has camera permission?', result.hasPermission),
-            err => this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.CAMERA)
-        );
-
+        // camera permissions
         this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.CAMERA, this.androidPermissions.PERMISSION.GET_ACCOUNTS]);
-      });
-
-      // geolocation permissions
-
-      this.geolocation.getCurrentPosition().then((resp) => {
-        // resp.coords.latitude
-        // resp.coords.longitude
-        console.log(resp.coords.latitude, resp.coords.longitude)
-      }).catch((error) => {
-        console.log('Error getting location', error);
+        // geolocation permissions
+        this.geolocation.getCurrentPosition().then((resp) => {
+          console.log(resp.coords.latitude, resp.coords.longitude)
+        }).catch((error) => {
+          console.log('Error getting location', error);
+        });
       });
 
       // get bus service api key
-
       this.apiKey = environment.busServiceApiKey;
-    }
-  }
 
-  ngOnInit() {
-    this.url = this.sanitizer.bypassSecurityTrustResourceUrl('/assets/ar-busstops/index.html?busServiceApiKey=' + this.apiKey);
+      // init iframe
+      this.url = this.sanitizer.bypassSecurityTrustResourceUrl('/assets/ar-busstops/index.html?busServiceApiKey=' + this.apiKey);
+    }
   }
 
 }
