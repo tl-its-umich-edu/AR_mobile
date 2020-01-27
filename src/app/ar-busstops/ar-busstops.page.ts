e42d949ee28b47cfbadbcaf590b32a1fca46b4c0
Alex Kim <akim8@emich.edu>
2020-01-27 15:55:09 -0500

    solve bugs with permissions and premature iframe loading

--- src/app/ar-busstops/ar-busstops.page.ts
+++ src/app/ar-busstops/ar-busstops.page.ts
@@ -16,39 +16,27 @@ export class ArBusstopsPage {
 
   url: SafeResourceUrl;
   apiKey: string;
+  loadIframe: boolean = false;
 
   constructor(private platform: Platform, private androidPermissions: AndroidPermissions, private geolocation: Geolocation, private sanitizer: DomSanitizer, private location:Location) {
+
     if (this.platform.is('cordova')) {
       let hasCamPerms:boolean;
       let hasGeoPerms:boolean;
 
       // get device permissions
       this.platform.ready().then(() => {
-        // request permissions
-        this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.CAMERA, this.androidPermissions.PERMISSION.GET_ACCOUNTS, this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION, this.androidPermissions.PERMISSION.ACCESS_FINE_LOCATION]);
-
-        // check permissions
-        this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.CAMERA).then((result) => {
-          hasCamPerms = true;
-        }).catch((error) => {
-          hasCamPerms = false;
+        this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.CAMERA, this.androidPermissions.PERMISSION.ACCESS_FINE_LOCATION]).then((result) => {
+          console.log('camera permission:', this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.CAMERA));
+          console.log('geolocation permission:', this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.ACCESS_FINE_LOCATION));
+          this.loadIframe = result;
+          if (result) {
+            // get bus service api key
+            this.apiKey = environment.busServiceApiKey;
+            // init iframe
+            this.url = this.sanitizer.bypassSecurityTrustResourceUrl('assets/ar-busstops/index.html?busServiceApiKey=' + this.apiKey);
+          }
         });
-        this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.ACCESS_FINE_LOCATION).then((result) => {
-          hasGeoPerms = true;
-        }).catch((error) => {
-          hasGeoPerms = false;
-        });
-
-        console.log('camera permission:', hasCamPerms);
-        console.log('geolocation permission:', hasGeoPerms);
-
-        if (hasCamPerms && hasGeoPerms || true) {
-          // get bus service api key
-          this.apiKey = environment.busServiceApiKey;
-    
-          // init iframe
-          this.url = this.sanitizer.bypassSecurityTrustResourceUrl('assets/ar-busstops/index.html?busServiceApiKey=' + this.apiKey);
-        }
       });
     }
   }
